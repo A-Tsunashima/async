66185438da17c72c7ceebe32fe2bf207c85fcef7
e1b23028 <e1b23028@st.oit.ac.jp>
2025-10-28 15:45:12 +0900

    sample5-2

--- src/main/java/oit/is/team53/kaizi/async/controller/Sample51Controller.java
+++ src/main/java/oit/is/team53/kaizi/async/controller/Sample51Controller.java
@@ -1,16 +1,34 @@
 package oit.is.team53.kaizi.async.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import oit.is.team53.kaizi.async.model.*;
+
+
 
 @Controller
 @RequestMapping("/sample5")
 public class Sample51Controller {
     
+    @Autowired
+    FruitMapper fMapper;
+
     @GetMapping("step1")
     public String step1() {
         return "sample51.html";
     }
+
+    @GetMapping("step2")
+    public String step2(ModelMap model) {
+        ArrayList<Fruit> fruits = fMapper.selectAllFruit();
+        model.addAttribute("fruits", fruits);
+        return "sample51.html";
+    } 
     
 }
---
+++ src/main/java/oit/is/team53/kaizi/async/model/Fruit.java
@@ -0,0 +1,41 @@
+package oit.is.team53.kaizi.async.model;
+
+public class Fruit {
+    int id;
+    String name;
+    int price;
+
+    public Fruit() {
+    
+    }
+
+    public Fruit(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
